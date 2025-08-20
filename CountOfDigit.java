6de3a2fa1f7ea1c7c233e02993ab652fa1853406
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-08-20 13:08:20 +0530

    Create CountOfDigit.java

---
+++ CountOfDigit.java
@@ -0,0 +1,20 @@
+package com.prity.practicequestion;
+import java.util.Scanner;
+
+public class CountOfDigit {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number ");
+		
+	   int num=sc.nextInt();
+	   int c=0;
+	   while(num!=0) {
+		   c++;
+		   num=num/10;
+	   }
+         System.out.println("Count of digits is "+c);
+         sc.close();
+	}
+
+}
