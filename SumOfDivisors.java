15776b7dc1b76e9f74d9e40221dc02dd20a87d4a
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-08-20 21:52:39 +0530

    Create SumOfDivisors.java

---
+++ SumOfDivisors.java
@@ -0,0 +1,21 @@
+package com.prity.practicequestion;
+import java.util.Scanner;
+public class SumOfDivisors {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1; i<=n; i++ ) {
+			if(n%i==0) {
+				sum=sum+i;
+			}
+		}
+		
+		System.out.println("Divisor of "+n+" is "+sum);
+		
+
+	}
+
+}
