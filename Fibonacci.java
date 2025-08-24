86b1bf7dcdbd7224f6928a1bd7de5b67a78985c0
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-08-24 18:47:46 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Fibonacci 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number of terms ");
+		int n = sc.nextInt();
+		
+		int f=0; 
+		int s=1;
+		int next;
+		System.out.println("Fibonacci series is");
+		
+		for(int i=0; i<=n; i++) {
+			System.out.println(f);
+			next = s+f;
+			f=s;
+			s=next;
+		}
+	}
+}
