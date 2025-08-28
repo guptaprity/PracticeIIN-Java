d7ca96263525ef670953778b683b5f74bd71a67f
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-08-28 21:46:26 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,8 @@
 package com.prity.practicequestion;
 import java.util.Scanner;
 public class Factorial {
-
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+public static void main(String[] args) {
+	  	Scanner sc= new Scanner(System.in);
 		System.out.print("Enter a number ");
 		int n=sc.nextInt();
 		int pro=1;
